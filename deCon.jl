77d61dc8e06f9cb44ac870025d0b4e36c81d9d5e
Jakub Ślęzak <128084860+jaksle@users.noreply.github.com>
2024-12-05 18:23:30 +0100

    Update deCon.jl

--- deCon.jl
+++ deCon.jl
@@ -28,3 +28,52 @@ heatmap(zs',
     axisratio=1,
     xlim=(1,200),
 )
+
+## Richardson-Lucy deconv
+
+conv(zs,ns) = real.(ifft( fft(zs) .* fft(ns)))
+function deconv(zs,ns,n)
+    ins = reverse(ns)
+    res = copy(zs)
+    for _ in 1:n
+        den = real.(ifft( fft(res) .* fft(ns)))
+        res .*= real.(ifft( fft(zs ./ den) .* fft(ins)))
+    end
+    return res
+end
+
+ins = reverse(ns) # for Gauss no change
+
+res = copy(zs)
+
+for _ in 1:100
+    den = real.(ifft( fft(res) .* fft(ns)))
+    res .*= real.(ifft( fft(zs ./ den) .* fft(ins)))
+end
+
+## singular case
+
+ws = real.(ifft(fft(ys) .* fft(ns)))
+
+ins = reverse(circshift(ws,(100,100)))
+
+res = copy(ys)
+
+for _ in 1:100
+    den = real.(ifft( fft(res) .* fft(ins)))
+    res .*= real.(ifft( fft(ys ./ den) .* fft(ins)))
+end
+
+##
+
+using Distributions
+
+p(x,y) = pdf(Gamma(2,1),x+1)*pdf(Normal(0,1),y)
+
+zs = p.(xs',xs)
+heatmap(zs)
+
+ws = conv(zs,ns)
+
+xs = deconv(ws,ns,100)
+heatmap(xs)
