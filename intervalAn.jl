cac4250348ba70e9c0a35fbedad0ff6c9054f0ec
Jakub Ślęzak <128084860+jaksle@users.noreply.github.com>
2025-02-04 02:05:17 +0100

    Update intervalAn.jl

--- intervalAn.jl
+++ intervalAn.jl
@@ -11,11 +11,11 @@ n = 10^4
 ln = 200
 ts = 1:ln
 
-K = (s,t) -> D0/2*(t^(2H0)+s^(2H0)-abs(s-t)^(2H0))
+K = (s,t) -> D0*(t^(2H0)+s^(2H0)-abs(s-t)^(2H0))
 S = [K(s,t) for s in ts, t in ts]
 A = cholesky(Symmetric(S)).U
 ξ = randn(length(ts), n)
-X = A'*ξ .+ cumsum(randn(ln,n),dims=1)
+#X = A'*ξ .+ cumsum(randn(ln,n),dims=1)
 X = cumsum(randn(ln,n),dims=1)
 ##
 
@@ -71,9 +71,131 @@ end
 
 gR = (Ts[w:end,:]'*Σ[w:end,w:end]^-1*Ts[w:end,:])^-1*Ts[w:end,:]'*Σ[w:end,w:end]^-1
 for i in 1:n
-    gB2[:,i] .= gR*(msd[w:end,i] +log(10)*diag(Σ[w:end,w:end])/2)
+    gB2[:,i] .= gR*(msd[w:end,i])
 end
 
 eO = (Ts[1:l,:]'*Ts[1:l,:])^-1*Ts[1:l,:]'*Σ[1:l,1:l]*Ts[1:l,:]*(Ts[1:l,:]'*Ts[1:l,:])^-1
 eO2 = (Ts[w:w+l,:]'*Ts[w:w+l,:])^-1*Ts[w:w+l,:]'*Σ[w:w+l,w:w+l]*Ts[w:w+l,:]*(Ts[w:w+l,:]'*Ts[w:w+l,:])^-1
 eG = (Ts[w:end,:]'*Σ[w:end,w:end]^-1*Ts[w:end,:])^-1
+
+## ta-msd plot
+
+k = 556
+errVar = diag(Σ)
+
+p = plot(
+    fontfamily = "Computer Modern",
+    xlabel = "t [T]",
+    ylabel = L"MSD [L$^2$]",
+    #xticks = (log10.(xt), string.(xt)),
+    #yticks = (log10.(yt), [L"10^{-3}",L"5\!\cdot\! 10^{-3}", L"10^{-2}", L"5\!\cdot\! 10^{-2}", L"10^{-1}",L"10^0" ]),
+    label = "",
+    legend = :top,
+    ylim = (1,400),
+    xlim = (1,201),
+    #xscale = :log10,
+    #yscale = :log10
+ )
+ scatter!([], [],
+    marker = :circle,
+    markercolor = :white,
+    markersize = 3,
+    #yerrors = sqrt.(2errVar),
+    label = "TA-MSD",
+)
+plot!(Shape([10,1,1,10],[550,550,1,1]),
+    color = palette(:default)[1],
+    alpha = 0.3,
+    linewidth = 0,
+    label = "window used for OLS1",
+)
+plot!(Shape([20,10,10,20],[550,550,1,1]),
+    color = palette(:default)[4],
+    alpha = 0.3,
+    linewidth = 0,
+    label = "window used for OLS2",
+)
+scatter!(ts[1:2:end], msd[1:2:end,k],
+    color = palette(:default)[3],
+    marker = :square,
+    yerrors = sqrt.(errVar[1:2:end]),
+    markersize = 0,
+    label = "",
+)
+
+scatter!(ts, msd[:,k],
+    marker = :circle,
+    markercolor = :white,
+    markersize = 3,
+    #yerrors = sqrt.(2errVar),
+    label = "",
+)
+
+plot!(t->B1[2,k]*t+B1[1,k],ts[1],4ts[l],
+    color = palette(:default)[1],
+    linestyle = :dash,
+    linewidth = 2.5,
+    label = "OLS1 fit"
+)
+plot!(t->B2[2,k]*t+B2[1,k],ts[w],maximum(ts),
+    color = palette(:default)[4],
+    linestyle = :dash,
+    linewidth = 2.5,
+    label = "OLS2 fit"
+)
+plot!(t->gB2[2,k]*t+gB2[1,k],ts[w],maximum(ts),
+    color = palette(:default)[3],
+    linestyle = :dash,
+    linewidth = 2.5,
+    label = "GLS fit"
+)
+
+
+## scatter plot
+
+p = plot(layout=(2,1))
+scatter!(p[1],B2[1,:],B2[2,:] .-1/2,
+    fontfamily = "Computer Modern",
+    ylabel = L"\hat D\ [L^2/T]",
+    xlabel = L"\hat  σ\ [L^2]",
+    markerstrokewidth=0,
+    markersize=2.0,
+    alpha = 0.3,
+    color = palette(:default)[4],
+    label = "OLS2",
+    xlim = (-20,20),
+    ylim = (-1,3)
+)
+scatter!(p[2],gB2[1,:],gB2[2,:] .-1/2,
+    fontfamily = "Computer Modern",
+    ylabel = L"\hat D\ [L^2/T]",
+    xlabel = L"\hat σ\ [L^2]",
+    markerstrokewidth=0,
+    markersize=2.0,
+    alpha = 0.3,
+    color = palette(:default)[3],
+    label = "GLS",
+    xlim = (-25,20),
+    ylim = (-1,3)
+)
+
+f(t) = cos(t)*sqrt(5.99) # 95% elipse
+g1(t) = sin(t)*sqrt(5.99)
+C = sqrt(eO2)
+plot!(p[1],t->C[1,1]*f(t)+C[1,2]*g1(t),t->C[2,1]*f(t)+C[2,2]*g1(t)+ 1/2,0,2pi,
+    linewidth = 1,
+    color = :black,
+    linestyle = :dash,
+    label = "error 95% ellipse",
+)
+
+C = sqrt(eG)
+plot!(p[2],t->C[1,1]*f(t)+C[1,2]*g1(t),t->C[2,1]*f(t)+C[2,2]*g1(t)+ 1/2,0,2pi,
+    linewidth = 1,
+    color = :black,
+    linestyle = :dash,
+    label = "error 95% ellipse",
+)
+
+scatter!(p[1], [0],[1/2],marker=:x,color=:red, label = L"exact $(\sigma, D)$")
+scatter!(p[2], [0],[1/2],marker=:x,color=:red, label = L"exact $(\sigma, D)$")
