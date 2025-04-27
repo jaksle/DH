d8bb8f67c40ab371606a397dc2d391aec225f41b
Jakub Ślęzak <128084860+jaksle@users.noreply.github.com>
2025-04-27 19:48:23 +0200

    Update simShort.jl

--- simShort.jl
+++ simShort.jl
@@ -7,11 +7,13 @@ include("funs.jl")
 
 ##
 
-H0, D0 = 0.75, 10^-2
+H0, D0 = 0.6, 10^-2
 n = 10^5
 ln = 10
 dt =  0.0567
 ts = dt*(1:ln)
+lts = log10.(ts[1:ln-1])
+Ts = [ones(ln-1) lts]
 
 K = (s,t) -> D0*(t^(2H0)+s^(2H0)-abs(s-t)^(2H0))
 S = [K(s,t) for s in ts, t in ts]
@@ -37,6 +39,31 @@ for k in eachindex(hs)
     bias[:,k] .=  -log(10) .* diag(errC[:,:,k]) ./4
 end
 
+## GLS prep exact
+
+hs = 0.05:0.01:0.8
+errCEx = Array{Float64}(undef,ln-1,ln-1,length(hs))
+biasEx = Array{Float64}(undef,ln-1,length(hs))
+msdTemp = Matrix{Float64}(undef,ln-1,n)
+
+@showprogress for k in eachindex(hs) # uwaga 1D czy 2D!
+    f = (s,t) -> 1*(t^(2hs[k])+s^(2hs[k])-abs(s-t)^(2hs[k])) 
+    S = [f(s,t) for s in ts, t in ts]
+    A = cholesky(Symmetric(S)).U
+    ξ = randn(length(ts), n)
+    X = A'*ξ
+    ξ = randn(length(ts), n)
+    Y = A'*ξ
+
+    for i in 1:n
+        msdTemp[:,i] .= estMSD(X[:,i],ln-1) .+ estMSD(Y[:,i],ln-1)  # 2D
+    end
+    ltemp = log10.(msdTemp)
+    errCEx[:,:,k] .= cov(ltemp')
+
+    biasEx[:,k] .=  mean(ltemp,dims = 2) .- 2hs[k]*lts .- log10(4)  # 2D bias
+end
+
 
 ## msd fit
  
@@ -47,9 +74,8 @@ end
 
 lmsd = log10.(msd)
 
-lts = log10.(ts[1:ln-1])
-Ts = [ones(ln-1) lts]
-l = 2
+
+l = 2 # window
 
 B = Matrix{Float64}(undef, 2, n)
 for i in 1:n
@@ -62,6 +88,7 @@ B[1,:] .-= log10(4)
 w = 9 # window
 gB = Matrix{Float64}(undef, 2, n)
 bB = Matrix{Float64}(undef, 2, n)
+eB = Matrix{Float64}(undef, 2, n)
 
 for i in 1:n
     j = findfirst(hs .>= B[2,i]/2) # H not α
@@ -69,18 +96,26 @@ for i in 1:n
     gR = (Ts[1:w,:]'*errC[1:w,1:w,j]^-1*Ts[1:w,:])^-1*Ts[1:w,:]'*errC[1:w,1:w,j]^-1
     gB[:,i] .= gR*lmsd[1:w,i]
     bB[:,i] .= gR*(lmsd[1:w,i] .- bias[1:w,j])
+
+    gR = (Ts[1:w,:]'*errCEx[1:w,1:w,j]^-1*Ts[1:w,:])^-1*Ts[1:w,:]'*errCEx[1:w,1:w,j]^-1
+    eB[:,i] .= gR*(lmsd[1:w,i] .- biasEx[1:w,j])
 end
 
 gB[1,:] .-= log10(4)
 bB[1,:] .-= log10(4)
+eB[1,:] .-= log10(4)
 
 ## perfect GLS
 w = 9
+H = H0
+
 pB = Matrix{Float64}(undef, 2, n)
+K = (s,t) -> 2D0*(t^(2H)+s^(2H)-abs(s-t)^(2H))
 errM =[theorCovEff(i,j,ln,K)/(K(ts[i],ts[i])*K(ts[j],ts[j])) * 1/(log(10)^2) for i in 1:ln-1, j in 1:ln-1]
-errM = cov(lmsd')
-errVar = diag(errM)
-b2 = @. -log(10)*errVar/4
+
+#errM = cov(lmsd')
+#errVar = diag(errM)
+b2 =  mean(lmsd,dims = 2) .- 2H*lts .+ log10(4D0)  # 2D
 gR = (Ts[1:w,:]'*errM[1:w,1:w]^-1*Ts[1:w,:])^-1*Ts[1:w,:]'*errM[1:w,1:w]^-1
 for i in 1:n
     #gB[:,i] .= gR*lmsd[1:w,i]
@@ -103,6 +138,55 @@ end
 ##
 
 
+var(bB[1,:])/var(B[1,:])
+var(eB[1,:])/var(B[1,:])
+var(pB[1,:])/var(B[1,:])
+
+var(bB[2,:])/var(B[2,:])
+var(eB[2,:])/var(B[2,:])
+var(pB[2,:])/var(B[2,:])
+
+mean(B[1,:])
+mean(gB[1,:])
+mean(bB[1,:])
+mean(eB[1,:])
+mean(pB[1,:])
+
+mean(B[2,:])
+mean(gB[2,:])
+mean(bB[2,:])
+mean(eB[2,:])
+mean(pB[2,:])
+
+
+
+mean(B[2,B[2,:] .> 0])
+mean(gB[2,gB[2,:] .> 0])
+mean(bB[2,bB[2,:] .> 0])
+mean(eB[2,eB[2,:] .> 0])
+
+count(B[2,:] .<= 0 )
+count(bB[2,:] .<= 0 )
+count(eB[2,:] .<= 0 )
+count(pB[2,:] .<= 0 )
+
+count(B[2,:] .>= 1 )
+count(pB[2,:] .>= 1 )
+## bias
+
+mlmsd = mean(lmsd,dims=2)
+
+plot(lts,2H0 .*lts .-2*log(2))
+plot!(lts,mlmsd .- b2) # 1D
+
+errM =[theorCovEff(i,j,ln,K)/(K(ts[i],ts[i])*K(ts[j],ts[j])) * 1/(log(10)^2) for i in 1:ln-1, j in 1:ln-1]
+errVar = diag(errM)
+b2 = @. -log(10)*errVar/4
+
+
+##
+
+
 scatter(B[1,:],B[2,:],
     markerstrokewidth=0,
     markersize=0.5,
@@ -118,38 +202,3 @@ scatter!(bB[1,:],bB[2,:],
     color = palette(:default)[3],
     label = "",
 )
-
-var(bB[1,:])/var(B[1,:])
-var(pB[1,:])/var(B[1,:])
-
-var(gB[2,:])/var(B[2,:])
-
-var(bB[2,:])/var(B[2,:])
-var(pB[2,:])/var(B[2,:])
-
-count(B[2,:] .<= 0 )
-count(pB[2,:] .<= 0 )
-
-mean(B[2,:])
-mean(gB[2,:])
-mean(bB[2,:])
-mean(pB[2,:])
-
-mean(B[1,:])
-mean(gB[1,:])
-mean(bB[1,:])
-mean(pB[1,:])
-
-mean(B[2,B[2,:] .> 0])
-mean(gB[2,B[2,:] .> 0])
-mean(pB[2,B[2,:] .> 0])
-## bias
-
-mlmsd = mean(lmsd,dims=2)
-
-plot(lts,2H0 .*lts .-2*log(2))
-plot!(lts,mlmsd .- b2) # 1D
-
-errM =[theorCovEff(i,j,ln,K)/(K(ts[i],ts[i])*K(ts[j],ts[j])) * 1/(log(10)^2) for i in 1:ln-1, j in 1:ln-1]
-errVar = diag(errM)
-b2 = @. -log(10)*errVar/4
