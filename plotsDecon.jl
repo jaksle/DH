51139e0e8869de817a754af0ff45ab5b53613e1c
Jakub Ślęzak <128084860+jaksle@users.noreply.github.com>
2025-05-05 01:01:33 +0200

    plotsDecon

--- plotsDecon.jl
+++ plotsDecon.jl
@@ -1,3 +1,8 @@
+using CairoMakie,  Distributions, LaTeXStrings, ProgressMeter, LinearAlgebra
+using KernelDensity, FFTW
+
+include("funs.jl")
+
 ## 1 population deconvolution
 
 n = 10^4
@@ -68,62 +73,19 @@ end
 gB[1,:] .-= log10(4)
 bB[1,:] .-= log10(4)
 
-##
+## num prep
 
-
-p1 = scatter(B[1,:],B[2,:],
-    markerstrokewidth=0,
-    markersize=0.5,
-    alpha = 0.3,
-    color = palette(:default)[1],
-    label = "",
-)
-
-p2 = scatter(bB[1,:],bB[2,:],
-    markerstrokewidth=0,
-    markersize=0.5,
-    alpha = 1,
-    color = palette(:default)[3],
-    label = "",
-)
-
-scatter!(p1, [log10(D0)],[2H0],marker=:x,color=:red, label = "")
-scatter!(p2, [log10(D0)],[2H0],marker=:x,color=:red, label = "")
-
-
-K = (s,t) -> D0*(t^(2H0)+s^(2H0)-abs(s-t)^(2H0))
+K = (s,t) -> 2D0*(t^(2H0)+s^(2H0)-abs(s-t)^(2H0))
 Σ = [2theorCovEff(i,i2,ln,K)/(2K(ts[i],ts[i])*2K(ts[i2],ts[i2]))* 1/(log(10)^2) for i in 1:ln-1,i2 in 1:ln-1] # factor 2!
-eM = (Ts'*Σ^-1*Ts)^-1
-eM2 = (Ts[1:l,:]'*Ts[1:l,:])^-1*Ts[1:l,:]'*Σ[1:l,1:l]*Ts[1:l,:]*(Ts[1:l,:]'*Ts[1:l,:])^-1
+eM = (Ts[1:l,:]'*Σ[1:l,1:l]^-1*Ts[1:l,:])^-1  # GLS
+eM2 = (Ts[1:l,:]'*Ts[1:l,:])^-1*Ts[1:l,:]'*Σ[1:l,1:l]*Ts[1:l,:]*(Ts[1:l,:]'*Ts[1:l,:])^-1 # OLS
 
 f(t) = cos(t)*sqrt(5.99) # 95% elipse
 g1(t) = sin(t)*sqrt(5.99)
 
-C = sqrt(eM2)
 
-plot!(p1, t->C[1,1]*f(t)+C[1,2]*g1(t) + log10(D0),t->C[2,1]*f(t)+C[2,2]*g1(t)+ 2H0,0,2pi,
-    linewidth = 1,
-    color = :black,
-    linestyle = :dash,
-    label = "",
-)
-C = sqrt(eM)
+den = kde((bB[1,:],bB[2,:]),boundary=((-0.2,0.2),(0.4,1.0)),npoints=(500,500) )
 
-plot!(p2, t->C[1,1]*f(t)+C[1,2]*g1(t) + log10(D0),t->C[2,1]*f(t)+C[2,2]*g1(t)+ 2H0,0,2pi,
-    linewidth = 1,
-    color = :black,
-    linestyle = :dash,
-    label = "",
-)
-
-
-den = kde((bB[1,:],bB[2,:]),boundary=((-0.2,-0.2),(0.4,1.0)),npoints=(500,500), )
-
-heatmap(den.x,den.y,den.density',xlim=(den.x[1],den.x[end]),ylim=(den.y[1],den.y[end]))
-hline!([0.7],color=:white, linestyle = :dash, label = "")
-vline!([0.0],color=:white, linestyle = :dash, label = "")
-
-savefig("oryg.png")
 
 nn= MvNormal([den.x[end÷2], den.y[end÷2]], Symmetric(eM))
 
@@ -139,8 +101,89 @@ for _ in 1:100
     res .*= real.(ifft( fft(zs ./ d) .* fft(ins)))
 end
 
-heatmap(den.x,den.y,res',xlim=(den.x[1],den.x[end]),ylim=(den.y[1],den.y[end]))
-hline!([0.7],color=:white, linestyle = :dash, label = "")
-vline!([0.0],color=:white, linestyle = :dash, label = "")
 
-savefig("rozplot.png")
+## plot
+
+darkRed = colorant"#cc3434"
+
+with_theme(theme_latexfonts()) do
+fig = Figure(size=(1200,400))
+xlab = [L"10^{%$i}" for i in -0.15:0.05:0.15]
+xlab[end÷2+1] = L"1"
+ax = Axis(fig[1,1],
+    xticks = (-0.15:0.05:0.15,xlab),
+    limits = (-0.15,0.15, 0.4,1),
+    title = "Step 1: points and predicted errors",
+    titlesize = 20,
+    xlabelsize= 20,
+    ylabelsize = 20,
+    xlabel = L"\hat{D}",
+    ylabel = L"\hat{\alpha}",
+    xgridvisible = false,
+    ygridvisible = false,
+)
+gls= scatter!(ax,bB[1,:],bB[2,:],
+    #markerstrokewidth=0,
+    markersize=8,
+    alpha = 0.15,
+    color = darkRed,
+    #label = "",
+
+)
+scatter!(ax, [log10(D0)],[2H0],
+    marker='⨉',
+    color=:black,
+    markersize = 15,
+)
+
+C = sqrt(eM)
+θs = LinRange(0,2pi,200)
+xs =  @. C[1,1]*f(θs)+C[1,2]*g1(θs) + log10(D0)
+ys = @. C[2,1]*f(θs)+C[2,2]*g1(θs)+ 2H0
+conf = lines!(ax,xs,ys ,
+    linewidth = 1.5,
+    color = :black,
+    linestyle = :dash,
+    label = "",
+)
+
+
+ax2 = Axis(fig[1,2],
+    xticks = (-0.15:0.05:0.15,xlab),
+    xlabel = L"\hat{D}",
+    limits = (-0.15,0.15, 0.4,1),
+    title = "Step 2: density estimate",
+    titlesize = 20,
+    xlabelsize= 20,
+    ylabelsize = 20,
+)
+heatmap!(ax2, den.x,den.y,den.density,
+    colormap = :thermal,
+)
+scatter!(ax2, [log10(D0)],[2H0],
+    marker='⨉',
+    color=:black,
+    markersize = 15,
+)
+
+ax3 = Axis(fig[1,3],
+    xticks = (-0.15:0.05:0.15,xlab),
+    xlabel = L"\hat{D}",
+    limits = (-0.15,0.15, 0.4,1),
+    title = "Step 3: deconvolution",
+    titlesize = 20,
+    xlabelsize= 20,
+    ylabelsize = 20,
+)
+heatmap!(ax3, den.x,den.y,res)
+cross = scatter!(ax3, [log10(D0)],[2H0],
+    marker='⨉',
+    color=:black,
+    markersize = 15,
+)
+
+axislegend(ax,[MarkerElement(color = darkRed, marker=:circle, alpha = 0.5, markersize = 12),  conf, cross,],["GLS","error 95% ellipse",L"exact ($D, \alpha$)",])
+fig
+save("decEx.pdf",fig)
+end
+
