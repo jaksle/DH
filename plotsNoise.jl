528f989c377adadab7f77d6598dd277ab09134a1
Jakub Ślęzak <128084860+jaksle@users.noreply.github.com>
2025-05-06 08:52:16 +0200

    Update plotsNoise.jl

--- plotsNoise.jl
+++ plotsNoise.jl
@@ -8,6 +8,7 @@ include("funs.jl")
 ##
 
 H0, D0 = 0.35, 1.
+σ = 5
 n = 10^5
 ln = 100
 dt =  1
@@ -15,13 +16,11 @@ ts = dt*(1:ln)
 lts = log10.(ts[1:ln-1])
 Ts = [ones(ln-1) lts]
 
-K = (s,t) -> D0*(t^(2H0)+s^(2H0)-abs(s-t)^(2H0))
-S = [K(s,t) for s in ts, t in ts]
+K = (s,t) -> D0*(t^(2H0)+s^(2H0)-abs(s-t)^(2H0)) 
+S = [K(s,t) for s in ts, t in ts] 
 A = cholesky(Symmetric(S)).U
 ξ = randn(length(ts), n)
-X = A'*ξ
-ξ = randn(length(ts), n)
-Y = A'*ξ
+X = A'*ξ .+ σ .* randn(ln,n)
 
 
 ## GLS prep
@@ -31,37 +30,12 @@ errC = Array{Float64}(undef,ln-1,ln-1,length(hs))
 bias = Array{Float64}(undef,ln-1,length(hs))
 
 @showprogress for k in eachindex(hs) # uwaga 1D czy 2D!
-    f = (s,t) -> 1*(t^(2hs[k])+s^(2hs[k])-abs(s-t)^(2hs[k])) # no ln(10)
+    f = (s,t) -> D0*(t^(2hs[k])+s^(2hs[k])-abs(s-t)^(2hs[k])) + ( (s==t) ? 2σ^2 : 0. )
     errC[:,:,k] .= [theorCovEff(i,j,ln,f)/(f(ts[i],ts[i])*f(ts[j],ts[j])) * 1/(log(10)^2) for i in 1:ln-1, j in 1:ln-1]
 end
 
 for k in eachindex(hs)
-    bias[:,k] .=  -log(10) .* diag(errC[:,:,k]) ./4
-end
-
-## GLS prep exact
-
-hs = 0.05:0.01:0.95
-errCEx = Array{Float64}(undef,ln-1,ln-1,length(hs))
-biasEx = Array{Float64}(undef,ln-1,length(hs))
-msdTemp = Matrix{Float64}(undef,ln-1,n)
-
-@showprogress for k in eachindex(hs) # uwaga 1D czy 2D!
-    f = (s,t) -> 1*(t^(2hs[k])+s^(2hs[k])-abs(s-t)^(2hs[k])) 
-    S = [f(s,t) for s in ts, t in ts]
-    A = cholesky(Symmetric(S)).U
-    ξ = randn(length(ts), n)
-    x = A'*ξ
-    ξ = randn(length(ts), n)
-    y = A'*ξ
-
-    for i in 1:n
-        msdTemp[:,i] .= estMSD(x[:,i],ln-1) .+ estMSD(y[:,i],ln-1)  # 2D
-    end
-    ltemp = log10.(msdTemp)
-    errCEx[:,:,k] .= cov(ltemp')
-
-    biasEx[:,k] .=  mean(ltemp,dims = 2) .- 2hs[k]*lts .- log10(4)  # 2D bias
+    bias[:,k] .=  -log(10) .* diag(errC[:,:,k]) ./2 # 1D
 end
 
 
@@ -69,7 +43,7 @@ end
  
 msd = Matrix{Float64}(undef,ln-1,n)
 for i in 1:n
-    msd[:,i] .= estMSD(X[:,i],ln-1) .+ estMSD(Y[:,i],ln-1) 
+    msd[:,i] .= estMSD(X[:,i],ln-1)
 end
 
 lmsd = log10.(msd)
@@ -89,7 +63,7 @@ B[1,:] .-= log10(4)
 w = 99 # window
 gB = Matrix{Float64}(undef, 2, n)
 bB = Matrix{Float64}(undef, 2, n)
-eB = Matrix{Float64}(undef, 2, n)
+#eB = Matrix{Float64}(undef, 2, n)
 
 for i in 1:n
     j = findfirst(hs .>= B[2,i]/2) # H not α
@@ -101,12 +75,12 @@ for i in 1:n
     bB[:,i] .= gR*(lmsd[1:w,i] .- bias[1:w,j])
 
     gR = (Ts[1:w,:]'*errCEx[1:w,1:w,j]^-1*Ts[1:w,:])^-1*Ts[1:w,:]'*errCEx[1:w,1:w,j]^-1
-    eB[:,i] .= gR*(lmsd[1:w,i] .- biasEx[1:w,j])
+    #eB[:,i] .= gR*(lmsd[1:w,i] .- biasEx[1:w,j])
 end
 
 gB[1,:] .-= log10(4)
 bB[1,:] .-= log10(4)
-eB[1,:] .-= log10(4)
+#eB[1,:] .-= log10(4)
 
 
 ##
@@ -114,32 +88,32 @@ eB[1,:] .-= log10(4)
 mean(B[1,:])
 mean(gB[1,:])
 mean(bB[1,:])
-mean(eB[1,:])
+#mean(eB[1,:])
 mean(pB[1,:])
 
 mean(B[2,:])
 mean(gB[2,:])
 mean(bB[2,:])
-mean(eB[2,:])
+#mean(eB[2,:])
 mean(pB[2,:])
 
 
 var(bB[1,:])/var(B[1,:])
-var(eB[1,:])/var(B[1,:])
+#var(eB[1,:])/var(B[1,:])
 var(pB[1,:])/var(B[1,:])
 
 var(bB[2,:])/var(B[2,:])
-var(eB[2,:])/var(B[2,:])
+#var(eB[2,:])/var(B[2,:])
 var(pB[2,:])/var(B[2,:])
 
 mean(B[2,B[2,:] .> 0])
 mean(gB[2,gB[2,:] .> 0])
 mean(bB[2,bB[2,:] .> 0])
-mean(eB[2,eB[2,:] .> 0])
+#mean(eB[2,eB[2,:] .> 0])
 
 count(B[2,:] .<= 0 )
 count(bB[2,:] .<= 0 )
-count(eB[2,:] .<= 0 )
+#count(eB[2,:] .<= 0 )
 count(pB[2,:] .<= 0 )
 
 count(B[2,:] .>= 1 )
