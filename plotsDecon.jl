b9d5aec9c90e29fadda91d6eca0cbff974f0330f
Jakub Ślęzak <128084860+jaksle@users.noreply.github.com>
2025-05-07 21:57:51 +0200

    Update plotsDecon.jl

--- plotsDecon.jl
+++ plotsDecon.jl
@@ -140,86 +140,87 @@ end
 darkRed = colorant"#cc3434"
 
 with_theme(theme_latexfonts()) do
-fig = Figure(size=(1200,400),figure_padding=(0,20,0,0))
-xlab = [L"10^{%$i}" for i in -3.4:0.2:-2.6]
-xlab[end÷2+1] = L"10^{-3}"
-xname = L"{D}\ [\mu m^2/s^\alpha]"
-ax = Axis(fig[1,1],
-    xticks = (-3.4:0.2:-2.6,xlab),
-    limits = (-3.4,-2.6, 0.4,1),
-    title = "Step 1: points and predicted errors",
-    titlesize = 20,
-    xlabelsize= 20,
-    ylabelsize = 20,
-    xlabel = xname,
-    ylabel = L"{\alpha}\ [1]",
-    xgridvisible = false,
-    ygridvisible = false,
-)
-gls = scatter!(ax,eB[1,:],eB[2,:],
-    #markerstrokewidth=0,
-    markersize=6,
-    alpha = 0.15,
-    color = :tomato, #darkRed,
-    #label = "",
+    fig = Figure(size=(1200,400),figure_padding=(0,20,0,0))
+    xlab = [L"10^{%$i}" for i in -3.4:0.2:-2.6]
+    xlab[end÷2+1] = L"10^{-3}"
+    xname = L"{D}\ [\mu m^2/s^\alpha]"
+    ax = Axis(fig[1,1],
+        xticks = (-3.4:0.2:-2.6,xlab),
+        limits = (-3.4,-2.6, 0.4,1),
+        title = "Step 1: points and predicted errors",
+        titlesize = 20,
+        xlabelsize= 20,
+        ylabelsize = 20,
+        xlabel = xname,
+        ylabel = L"{\alpha}\ [1]",
+        xgridvisible = false,
+        ygridvisible = false,
+    )
+    gls = scatter!(ax,eB[1,:],eB[2,:],
+        #markerstrokewidth=0,
+        markersize=6,
+        alpha = 0.15,
+        color = :tomato, #darkRed,
+        #label = "",
 
-)
-scatter!(ax, [log10(D0)],[2H0],
-    marker='⨉',
-    color=:black,
-    markersize = 15,
-)
+    )
+    scatter!(ax, [log10(D0)],[2H0],
+        marker='⨉',
+        color=:black,
+        markersize = 15,
+    )
 
-C = sqrt(eM)
-θs = LinRange(0,2pi,200)
-xs =  @. C[1,1]*f(θs)+C[1,2]*g1(θs) + log10(D0)
-ys = @. C[2,1]*f(θs)+C[2,2]*g1(θs)+ 2H0
-conf = lines!(ax,xs,ys ,
-    linewidth = 1.5,
-    color = :black,
-    linestyle = :dash,
-    label = "",
-)
+    C = sqrt(eM)
+    θs = LinRange(0,2pi,200)
+    xs =  @. C[1,1]*f(θs)+C[1,2]*g1(θs) + log10(D0)
+    ys = @. C[2,1]*f(θs)+C[2,2]*g1(θs)+ 2H0
+    conf = lines!(ax,xs,ys ,
+        linewidth = 1.5,
+        color = :black,
+        linestyle = :dash,
+        label = "",
+    )
 
 
-ax2 = Axis(fig[1,2],
-    xticks = (-3.4:0.2:-2.6,xlab),
-    xlabel = xname,
-    limits = (-3.4,-2.6, 0.4,1),
-    title = "Step 2: density estimate",
-    titlesize = 20,
-    xlabelsize= 20,
-    ylabelsize = 20,
-)
-heatmap!(ax2, den.x,den.y,den.density,
-    colormap = :thermal,
-)
-scatter!(ax2, [log10(D0)],[2H0],
-    marker='⨉',
-    color=:black,
-    markersize = 15,
-)
+    ax2 = Axis(fig[1,2],
+        xticks = (-3.4:0.2:-2.6,xlab),
+        xlabel = xname,
+        limits = (-3.4,-2.6, 0.4,1),
+        title = "Step 2: density estimate",
+        titlesize = 20,
+        xlabelsize= 20,
+        ylabelsize = 20,
+    )
+    heatmap!(ax2, den.x,den.y,den.density,
+        colormap = :thermal,
+    )
+    scatter!(ax2, [log10(D0)],[2H0],
+        marker='⨉',
+        color=:black,
+        markersize = 15,
+    )
 
-ax3 = Axis(fig[1,3],
-    xticks = (-3.4:0.2:-2.6,xlab),
-    xlabel = xname,
-    limits = (-3.4,-2.6, 0.4,1),
-    title = "Step 3: deconvolution",
-    titlesize = 20,
-    xlabelsize= 20,
-    ylabelsize = 20,
-)
-heatmap!(ax3, den.x,den.y,res)
-cross = scatter!(ax3, [log10(D0)],[2H0],
-    marker='⨉',
-    color=:black,
-    markersize = 15,
-)
+    ax3 = Axis(fig[1,3],
+        xticks = (-3.4:0.2:-2.6,xlab),
+        xlabel = xname,
+        limits = (-3.4,-2.6, 0.4,1),
+        title = "Step 3: deconvolution",
+        titlesize = 20,
+        xlabelsize= 20,
+        ylabelsize = 20,
+    )
+    heatmap!(ax3, den.x,den.y,res)
+    cross = scatter!(ax3, [log10(D0)],[2H0],
+        marker='⨉',
+        color=:black,
+        markersize = 15,
+    )
 
-axislegend(ax,[MarkerElement(color = :tomato, marker=:circle, alpha = 0.6, markersize = 12),  conf, cross,],["GLS","error 95% ellipse",L"exact ($D, \alpha$)"],
-    position = :lt,
-)
-fig
-save("decEx.pdf",fig)
+    axislegend(ax,[MarkerElement(color = :tomato, marker=:circle, alpha = 0.6, markersize = 12),  conf, cross,],["GLS","error 95% ellipse",L"exact ($D, \alpha$)"],
+        position = :lt,
+    )
+    fig
+    #save("decEx.pdf",fig)
 end
 
+## data plots multiple 
