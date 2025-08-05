a310aea6d75a24f4139ecd7e30665b11ac12b772
Jakub Ślęzak <128084860+jaksle@users.noreply.github.com>
2025-08-05 16:04:32 +0200

    anDiff

--- anDiffTest.jl
+++ anDiffTest.jl
@@ -16,7 +16,6 @@ gls = fit_internal(msd, 2, dt, init_A)
 n = 10^4
 h = 0.3
 ts = 1:100
-ln = 100
 dt = step(ts)
 
 f = (s,t) -> 1*(t^(2h)+s^(2h)-abs(s-t)^(2h)) # D = 1
@@ -26,7 +25,10 @@ A = cholesky(Symmetric(S)).U
 X = A'*ξ
 ξ = randn(length(ts), n)
 Y = A'*ξ
-msd = tamsd([X ;;; Y])
+msd = Matrix{Float64}(undef,ln-1,n)
+for i in 1:n
+    msd[:,i] .= estMSD(X[:,i],ln-1) .+ estMSD(Y[:,i],ln-1)
+end
 
 gls, c = fit_gls(msd, 2, dt, fill(0.6,n))
 
@@ -40,7 +42,10 @@ gls2, c2 = fit_gls(msd[:,1:200], 2, dt, fill(0.6,n), precompute=false)
 
 σ = 1.0
 H0, D0 = 0.6, 5.
-n = 10^5
+n, ln = 10^4, 100
+ts = 1:100
+dt = step(ts)
+
 f = (s,t) -> D0*(t^(2H0)+s^(2H0)-abs(s-t)^(2H0)) 
 S = [f(s,t) for s in ts, t in ts]
 A = cholesky(Symmetric(S)).U
@@ -48,10 +53,11 @@ A = cholesky(Symmetric(S)).U
 X = A'*ξ .+ σ .* randn(ln,n)
 ξ = randn(length(ts), n)
 Y = A'*ξ .+ σ .* randn(ln,n)
-msd = Matrix{Float64}(undef,ln-1,n)
-for i in 1:n
-    msd[:,i] .= estMSD(X[:,i],ln-1) .+ estMSD(Y[:,i],ln-1)
-end
+# msd = Matrix{Float64}(undef,ln-1,n)
+# for i in 1:n
+#     msd[:,i] .= estMSD(X[:,i],ln-1) .+ estMSD(Y[:,i],ln-1)
+# end
+msd = tamsd([X ;;; Y])
 lg(x) = x >= 0 ? log10(x) : NaN
 lmsd = lg.(msd .- 2*2*σ^2)
 lmsd[isnan.(lmsd)] .= 0.
@@ -67,7 +73,10 @@ errC0 = @. 1/(log(10)^2) * (D0^2*orgC + σ^2*D0*crossC + σ^4*2*noiseC)/((2D0*2*
 
 gls1, c1 = fit_gls(msd, 2, dt, fill(α0,n))
 gls2, c2 = fit_gls(msd, 2, dt, fill(α0,n), fill(D0,n),σ)
-gls3, c3 = fit_gls(msd[:,1:500], 2, dt, fill(α0,n), fill(D0,n),σ, precompute = false)
+gls3, c3 = fit_gls(msd[:,1:1], 2, dt, fill(α0,n), fill(D0,n),σ, precompute = false)
+
+mean(gls2,dims=2)
+cov(gls3')
 
 Ts = [ones(ln-1) log10.(ts[1:ln-1])]
 parErr = (Ts'*inv(errC0)*Ts)^-1
