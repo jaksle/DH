b847a5eaf14f23c038aee296cb4aae28f94f0843
Jakub Ślęzak <128084860+jaksle@users.noreply.github.com>
2025-10-11 00:45:57 +0200

    Update dataGen.jl

--- dataGen.jl
+++ dataGen.jl
@@ -1,1 +1,25 @@
 using DataFrames, CSV, LinearAlgebra
+
+##
+
+
+H, D = 0.4, 1 # FBM parameters: Hurst index H and diffusivity D
+n = 10^3 # number of trajectories
+ln = 100 # trajectory length
+dt = 1. # time interval
+ts = dt*(1:ln)
+
+K = (s,t) -> D*(t^(2H)+s^(2H)-abs(s-t)^(2H)) # FBM covariance
+S = [K(s,t) for s in ts, t in ts]
+A = cholesky(Symmetric(S)).U
+
+ξ = randn(length(ts), n)
+X = A'*ξ
+ξ = randn(length(ts), n)
+Y = A'*ξ
+
+d = []
+for k in 1:n
+    push!(d,"X$k" => X[:,k])
+    push!(d,"Y$k" => Y[:,k])
+end
