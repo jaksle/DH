aabd6e268924e0fd14b32b2f4919a8b6cd0fc36c
Jakub Ślęzak <128084860+jaksle@users.noreply.github.com>
2026-04-07 20:51:31 +0200

    Create GLS.jl

---
+++ GLS.jl
@@ -0,0 +1,33 @@
+using QuadGK, SpecialFunctions
+using LinearAlgebra
+using CairoMakie
+##
+
+function spectrum(ω,s, t, H, ζ) # ω > 0, m = 1, kBT = 1
+    g = gamma(2H+1)
+    return (cos((t-s)*ω) - cos(t*ω) - cos(s*ω) + 1  ) * ω^(-2) *  ζ*2g*sinpi(H)*ω^(1-2H) / abs(ζ*g*ω^(1-2H)*(sinpi(H) - im*cospi(H)) - im*ω)^2
+end
+
+function covGLS(s, t, H, ζ) # ω > 0, m = 1, kBT = 1
+    2quadgk(ω->spectrum(ω,s, t, H, ζ),0, Inf)[1]
+end
+
+##
+
+ts = 1:100
+H, ζ = 0.6,  0.1
+C = Symmetric([covGLS(s,t, H, ζ) for s in ts, t in ts])
+
+##
+fig = Figure()
+ax = Axis(fig[1,1],
+    xscale = log10,
+    yscale = log10,
+)
+
+scatter!(ax, ts,diag(C))
+lines!(ax,ts, ζ^-1*sinpi(2H)/(pi*H*(1-2H)*(2-2H)) .* ts .^ (2-2H))
+#lines!(ax,ts, ts .^ 2)
+fig
+
+##
